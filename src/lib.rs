78d7a95072bf66eb768384ffb111e0b5aa57bd6e
Seb Ospina <kraige@gmail.com>
2023-04-16 22:04:51 +0200

    Starting filtering of events handling via Cli
    Signed-off-by: Seb Ospina <kraige@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
 //! Starcraft 2 - Replay visualizer.
 //!
 
-use nom_mpq::{parser, MPQ};
+use nom_mpq::{parser, MPQParserError, MPQ};
 use rerun::components::ColorRGBA;
-
+use rerun::external::re_log_types::DataTableError;
+use rerun::external::re_viewer::external::eframe::Error as eframe_Error;
+use rerun::time::Timeline;
+use rerun::Session;
+use rerun::{time, MsgSenderError};
+use std::collections::HashMap;
+pub use tracker_events::*;
 pub mod unit_colors;
 pub use unit_colors::*;
 pub mod game_events;
 pub use game_events::*;
 pub mod tracker_events;
-pub use tracker_events::*;
 
 /// The game events seem to be at this ratio when compared to Tracker Events.
 pub const GAME_EVENT_POS_RATIO: f32 = 27_000f32;
@@ -46,4 +51,83 @@ pub fn read_mpq(path: &str) -> (MPQ, Vec<u8>) {
     let file_contents = parser::read_file(path);
     let (_, mpq) = parser::parse(&file_contents).unwrap();
     (mpq, file_contents)
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum SwarmyError {
+    #[error("Rerun Message Sender error")]
+    RerunMsgSender(#[from] MsgSenderError),
+    #[error("Rerun Data Table Error")]
+    RerunDataTable(#[from] DataTableError),
+    #[error("Rerun Eframe Error")]
+    RerunEframe(#[from] eframe_Error),
+    #[error("MPQ Error")]
+    MPQ(#[from] MPQParserError),
+}
+
+/// Unit Attributes.
+#[derive(Debug, Default)]
+pub struct SC2Unit {
+    /// The last time the unit was updated
+    last_game_loop: i64,
+    /// The owner user_id
+    user_id: Option<i64>,
+    /// The name of the unit.
+    name: Option<String>,
+    /// The X position.
+    x: f32,
+    /// The Y position.
+    y: f32,
+    /// The Z position.
+    z: f32,
+    /// The age in game_loops of the unit.
+    age: f32,
+}
+
+pub struct SC2Replay {
+    /// The registered units state as they change through time.
+    units: HashMap<i64, SC2Unit>,
+    /// The absolute GameEvevnt loop timeline, the tracker loop should be relative to it.
+    timeline: Timeline,
+    /// The rerun session to display data.
+    rerun_session: Session,
+    /// The MPQ file being read.
+    mpq: MPQ,
+    /// The contents of the file
+    file_contents: Vec<u8>,
+}
+impl SC2Replay {
+    pub fn new(file_path: &str) -> Result<Self, SwarmyError> {
+        let rerun_session = rerun::SessionBuilder::new("swarmy-rerun").buffered();
+        let (mpq, file_contents) = read_mpq(file_path);
+        let timeline = rerun::time::Timeline::new("game_timeline", time::TimeType::Sequence);
+        Ok(Self {
+            units: HashMap::new(),
+            timeline,
+            rerun_session,
+            mpq,
+            file_contents,
+        })
+    }
+
+    pub fn add_events(&mut self) -> Result<usize, SwarmyError> {
+        let mut total_events = 0usize;
+        total_events += add_game_events(
+            &self.mpq,
+            &self.file_contents,
+            &mut self.session,
+            &self.timeline,
+        )?;
+        total_events += add_tracker_events(
+            &self.mpq,
+            &self.file_contents,
+            &mut self.session,
+            &self.timeline,
+        )?;
+        Ok(total_events)
+    }
+
+    pub fn show(&self) -> Result<(), SwarmyError> {
+        Ok(rerun::native_viewer::show(&self.rerun_session)?)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use rerun::time;
 use swarmy::*;
 use tracing_subscriber;
 
@@ -9,22 +8,37 @@ use rerun::external::re_memory::AccountingAllocator;
 static GLOBAL: AccountingAllocator<mimalloc::MiMalloc> =
     AccountingAllocator::new(mimalloc::MiMalloc);
 
+// TODO: There a ratio between tracker events and game events.
+// This ratio is still to be verified.
+
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Sets a custom config file
+    /// Source file, the SC2Replay extension usually.
     #[arg(short, long, value_name = "FILE")]
     source: String,
+
+    /// Filters a specific user id.
+    #[arg(short, long, value_name = "USER_ID")]
+    filter_user_id: Option<i64>,
+
+    /// Filters a specific unit tag.
+    #[arg(short, long, value_name = "UNIT_TAG")]
+    filter_unit_tag: Option<i64>,
+
+    /// Allows setting up a min event loop, in game_event units
+    #[arg(short, long, value_name = "MIN")]
+    min: Option<i64>,
+
+    /// Allows setting up a max event loop
+    #[arg(short, long, value_name = "MAX")]
+    max: Option<i64>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
     let cli = Cli::parse();
-    let mut session = rerun::SessionBuilder::new("swarmy-rerun").buffered();
-    let (mpq, file_contents) = read_mpq(&cli.source);
-    let game_timeline = rerun::time::Timeline::new("game_timeline", time::TimeType::Sequence);
-    add_game_events(&mpq, &file_contents, &mut session, &game_timeline)?;
-    add_tracker_events(&mpq, &file_contents, &mut session, &game_timeline)?;
-    rerun::native_viewer::show(&session)?;
+    let mut sc2_replay = SC2Replay::new(&cli.source)?;
+    sc2_replay.show()?;
     Ok(())
 }
